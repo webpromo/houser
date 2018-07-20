be9e8a7e83aa2459e372cf17870988233ebe0c66
Jesse <rank_booster@yahoo.com>
2018-07-20 10:29:20 -0600

    We're rockin' now!

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const bodyParser = require('body-parser');
 const controller = require('./controller');
 const massive = require('massive');
 require('dotenv').config();
-
 const app = express();
-// massive( process.env.CONNECTION_STRING ).then( dbInstance => app.set('db', dbInstance) );
+massive( process.env.CONNECTION_URI ).then( dbInstance => app.set('db', dbInstance) );
 app.use( bodyParser.json() );
+
+
 
 const port = process.env.SERVER_PORT || 3000;  // nodemon monitors this.
 app.listen( port, () => { console.log(`Server listening INTENTLY on port ${port}.`); } );
---
+++ src/route.js
@@ -0,0 +1,10 @@
+import {HashRouter,Switch,Route} from 'react-router-dom';
+import {Dashboard} from './component/Dashboard/Dashboard';
+import {Wizard} from './component/Wizard/Wizard';
+
+export default (
+    <Switch>
+      <Route exact path="/" component={ Dashboard } />
+      <Route path="/wizard" component={ Wizard } />
+    </Switch>
+  )
